d7cf8dd7310c7a1e7f97f9429ba92c10d7c713ef
Manish Choudhary <me.developer4u@gmail.com>
2026-06-12 18:34:42 +0530

    Hide Crucial Details

--- 04-Mongoose/src/db/db.js
+++ 04-Mongoose/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     
-    await mongoose.connect("mongodb://Backend-Learned:9355342840Ravi@ac-jhpahny-shard-00-00.lbyuace.mongodb.net:27017,ac-jhpahny-shard-00-01.lbyuace.mongodb.net:27017,ac-jhpahny-shard-00-02.lbyuace.mongodb.net:27017/?ssl=true&replicaSet=atlas-7hoxyh-shard-0&authSource=admin&appName=Backend-Learned");
+    await mongoose.connect("YOUR_MONGODB_URI");
     console.log("Connected to DB")
 };
 
