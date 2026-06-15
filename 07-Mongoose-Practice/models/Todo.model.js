2c4860187f646811dc0ea97413656d91f63eed7b
Manish Choudhary <me.developer4u@gmail.com>
2026-06-15 22:16:06 +0530

    MongooseDB Model Practice

---
+++ 07-Mongoose-Practice/models/Todo.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    completed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export const Todo = mongoose.model('Todo', todoSchema);
